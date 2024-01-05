6d56d86b25eb9464a09e23dc204023a474e1a845
AlvaroHG <alvaroherrasti@gmail.com>
2024-01-04 16:20:21 -0800

    Remove None concat error

--- objathor/asset_conversion/util.py
+++ objathor/asset_conversion/util.py
@@ -404,7 +404,6 @@ def create_asset(
     )
     if file_extension not in EXTENSIONS_LOADABLE_IN_UNITY:
         load_file_in_unity = False
-    print("--------- extension " + extension)
     copy_to_dir = (
         os.path.join(thor_controller._build.base_dir)
         if copy_to_dir is None
