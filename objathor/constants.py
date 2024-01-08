21f344771bd3cdc5809d945d865e15a6eceef9dc
AlvaroHG <alvaroherrasti@gmail.com>
2024-01-08 15:29:04 -0800

    Updates thor version

--- objathor/constants.py
+++ objathor/constants.py
@@ -2,4 +2,4 @@ import os
 from pathlib import Path
 
 ABS_PATH_OF_OBJATHOR = os.path.abspath(os.path.dirname(Path(__file__)))
-THOR_COMMIT_ID = "e24aa88d86d460f1f5352e28b8e754c3f5966865"
+THOR_COMMIT_ID = "13ef2aba9d0228c30d775cbae0b674f0826a97f2"
