5732209c09c56ecfc4f3a85e3b42a8234b18e473
AlvaroHG <alvaroherrasti@gmail.com>
2024-01-17 16:59:08 -0800

    format

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
             ]
 
     REQUIREMENTS = _read_reqs("requirements.txt")
-    REQUIREMENTS_ANNOTATION = _read_reqs("requirements-annotation.txt")
     REQUIREMENTS_BLENDER = _read_reqs("requirements-blender.txt")
+    REQUIREMENTS_ANNOTATION = _read_reqs("requirements-annotation.txt")
 
     setup(
         name="objathor",
@@ -35,9 +35,8 @@ if __name__ == "__main__":
         keywords=["3D assets", "annotation", ""],
         install_requires=REQUIREMENTS,
         extras_require={
-            "all": REQUIREMENTS_ANNOTATION + REQUIREMENTS_BLENDER,
             "blender": REQUIREMENTS_BLENDER,
-            "annotation": REQUIREMENTS_ANNOTATION,
+            "annotation": REQUIREMENTS_ANNOTATION
         },
         classifiers=[
             "Development Status :: 4 - Beta",
